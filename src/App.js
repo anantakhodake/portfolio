3a17d7d542237b1ba7d4952a1de6cf170f68af69
ananta khodake <ananta.khodake02@gmail.com>
2024-04-11 11:54:26 +0530

    delete a component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
-import MobileNavbar from "./components/MobileNavbar";
-import Introduction from "./components/Introduction";
-import Contact from "./components/Contact";
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <MobileNavbar />
-      <Introduction />
-      {/* <Skills /> */}
-      <Contact />
+      <h1>ananta</h1>
     </div>
   );
 }
